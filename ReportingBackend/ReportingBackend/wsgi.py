1059fd27d080aa4a3d4c9d7c07a8af1d073ca05b
Zineb <zack.kun16@gmail.com>
2025-06-17 16:53:53 +0100

    10 commit

--- ReportingBackend/ReportingBackend/wsgi.py
+++ ReportingBackend/ReportingBackend/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.getenv('DJANGO_SETTINGS_MODULE','ReportingBackend.settings.dev'))
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.getenv('DJANGO_SETTINGS_MODULE','ReportingBackend.settings.prod'))
 
 application = get_wsgi_application()
